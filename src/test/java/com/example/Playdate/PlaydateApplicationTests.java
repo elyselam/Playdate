48038a077f985efaa910f4cd113b8955b41eae8e
Elyse Lam <30991021+elyselam@users.noreply.github.com>
2020-08-06 18:37:18 -0700

    first

---
+++ src/test/java/com/example/Playdate/PlaydateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Playdate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaydateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
