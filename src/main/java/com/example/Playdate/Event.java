793e2935c36a58fd15d7d7f222063164304e8dbc
Elyse Lam <30991021+elyselam@users.noreply.github.com>
2020-08-08 12:48:33 -0700

    added controller

---
+++ src/main/java/com/example/Playdate/Event.java
@@ -0,0 +1,10 @@
+package com.example.Playdate;
+
+import java.time.LocalDate;
+
+public class Event {
+//    LocalDate time = LocalDate.now();
+//    String location;
+//    String description;
+
+}
---
+++ src/main/java/com/example/Playdate/User.java
@@ -0,0 +1,13 @@
+package com.example.Playdate;
+
+public class User {
+    int userId;
+    String email;
+    String phoneNumber;
+
+    public User(int userId, String email, String phoneNumber) {
+        this.userId = userId;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+}
