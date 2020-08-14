cc68122b62888139fcf0ab4e6095081f4fdb0cff
Elyse Lam <30991021+elyselam@users.noreply.github.com>
2020-08-13 19:41:53 -0700

    added Firebase Initializer

--- src/main/java/com/example/Playdate/Service/FirebaseInitializer.java
+++ src/main/java/com/example/Playdate/Service/FirebaseInitializer.java
@@ -1,23 +1,35 @@
 package com.example.Playdate.Service;
 
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
+import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+@Service
 public class FirebaseInitializer {
+
+    @PostConstruct
     private void initDB() throws IOException {
         FileInputStream serviceAccount =
-                new FileInputStream("path/to/serviceAccountKey.json");
+                new FileInputStream("../resources/playdate-50067-firebase-adminsdk-j1iko-522508380e.json");
 
         FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 .setDatabaseUrl("https://playdate-50067.firebaseio.com")
                 .build();
+        if (FirebaseApp.getApps().isEmpty()) {
+            FirebaseApp.initializeApp(options);
+        }
+    }
 
-        FirebaseApp.initializeApp(options);
+    public Firestore getFirebase() {
+        return FirestoreClient.getFirestore();
     }
 }
